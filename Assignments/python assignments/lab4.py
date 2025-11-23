b660f86c11b208cb87bfbbc34b0fcfe1a5236b14
AadityaSingh-arch <singhaditya.bagri@gmail.com>
2025-11-23 21:24:04 +0530

    Delete Assignments directory

--- Assignments/python assignments/lab4.py
+++
@@ -1,8 +0,0 @@
-from library_manager.book import Book
-from library_manager.inventory import Library
-from library_manager.cli import main
-# from library_manager.data.library_catalog import library_catalog
-def main():
-    library = Library()
-    library.load_catalog('library_catalog.txt') 
-    
